0efd6c2ab0173cd5cdde0fe5f7b7939c535229c6
dongmin-Lim <ldm96216@gmail.com>
2022-07-10 01:45:20 +0900

    Update 글꼴, 다크모드 error, css 여백

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function darkModeHandler() {
     // 다크 => 주간
     document.querySelector(".main").style.color = "black";
     document.querySelector(".nav").style.backgroundColor = "rgba(100, 100, 100, 0.7)";
-    document.querySelector(".nav").style.color = "white";
     document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
     document.querySelector(".dark_mode").style.backgroundColor =
       "rgba(100, 100, 100, 0.7)";
     document.querySelector(".dark_mode").style.color = "white";
@@ -14,6 +14,7 @@ function darkModeHandler() {
     document.querySelector(".main").style.color = "white";
     document.querySelector(".nav").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     document.querySelector("body").style.backgroundColor = "rgb(40, 40, 40)";
+    document.querySelector("body").style.color = "white";
     document.querySelector(".dark_mode").style.backgroundColor =
       "rgba(255, 255, 255, 0.6)";
     document.querySelector(".dark_mode").style.color = "black";
