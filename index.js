bfb91f293d7a7fc90fabbfc5aa39290411b8ae17
dongmin-Lim <ldm96216@gmail.com>
2022-07-10 21:35:39 +0900

    Update ahchorTag color 다크모드 변환시 에러 수정

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function darkModeHandler() {
+  const anchorTag = document.querySelectorAll("a");
   if (document.querySelector(".dark_mode").innerHTML == "주간모드") {
     // 다크 => 주간
+    for (var i = 0; i < anchorTag.length; i++) {
+      // my-tag 태그들의 글자 색을 하얀색으로 정한다.
+      anchorTag[i].style.color = "black";
+    }
     document.querySelector("body").style.backgroundColor = "white";
     document.querySelector("body").style.color = "black";
     document.querySelector(".dark_mode").style.backgroundColor =
@@ -9,6 +14,10 @@ function darkModeHandler() {
     document.querySelector(".right_profile").style.borderLeft = "3px solid black";
   } else if (document.querySelector(".dark_mode").innerHTML == "다크모드") {
     // 주간 => 다크
+    for (var i = 0; i < anchorTag.length; i++) {
+      // my-tag 태그들의 글자 색을 하얀색으로 정한다.
+      anchorTag[i].style.color = "white";
+    }
     document.querySelector("body").style.backgroundColor = "rgb(40, 40, 40)";
     document.querySelector("body").style.color = "white";
     document.querySelector(".dark_mode").style.backgroundColor =
