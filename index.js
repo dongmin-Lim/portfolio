45ed2d847a0c4ebe804fd79a94285e3a3ad79962
dongmin-Lim <ldm96216@gmail.com>
2022-07-10 21:30:43 +0900

    Update 오류 수정

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 function darkModeHandler() {
   if (document.querySelector(".dark_mode").innerHTML == "주간모드") {
     // 다크 => 주간
-    document.querySelector(".main").style.color = "black";
-    document.querySelector(".nav").style.backgroundColor = "rgba(100, 100, 100, 0.7)";
     document.querySelector("body").style.backgroundColor = "white";
     document.querySelector("body").style.color = "black";
     document.querySelector(".dark_mode").style.backgroundColor =
-      "rgba(100, 100, 100, 0.7)";
-    document.querySelector(".dark_mode").style.color = "white";
-    document.querySelector(".info_1").style.color = "black";
-    document.querySelector(".right_info").style.borderColor = "black";
-    document.querySelector(".a1").style.color = "black";
-    document.querySelector(".a2").style.color = "black";
+      "rgba(100, 100, 100, 0.6)";
     document.querySelector(".dark_mode").innerHTML = "다크모드";
+    document.querySelector(".right_profile").style.borderLeft = "3px solid black";
   } else if (document.querySelector(".dark_mode").innerHTML == "다크모드") {
     // 주간 => 다크
-    document.querySelector(".main").style.color = "white";
-    document.querySelector(".nav").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     document.querySelector("body").style.backgroundColor = "rgb(40, 40, 40)";
     document.querySelector("body").style.color = "white";
     document.querySelector(".dark_mode").style.backgroundColor =
       "rgba(255, 255, 255, 0.6)";
-    document.querySelector(".dark_mode").style.color = "black";
-    document.querySelector(".info_1").style.color = "white";
-    document.querySelector(".right_info").style.borderColor = "white";
-    document.querySelector(".a1").style.color = "white";
-    document.querySelector(".a2").style.color = "white";
     document.querySelector(".dark_mode").innerHTML = "주간모드";
+    document.querySelector(".right_profile").style.borderLeft = "3px solid white";
   }
 }
