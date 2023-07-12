8693e6e168470428f4b6c7c4fc3ab779d44ffd73
Dongmin-Lim <ldm96216@gmail.com>
2023-07-12 21:39:22 +0900

    Nav styled-components 적용

--- frontend/src/Component/Nav.js
+++ frontend/src/Component/Nav.js
@@ -1,15 +1,37 @@
-import styles from "./Nav.module.css";
+import styled from "styled-components";
 
 function Nav() {
   return (
-    <nav className={styles.nav}>
+    <Frame>
       <a href="/">HOME</a>
       <a href="/profile">PROFILE</a>
       <a href="/tech">TECH</a>
       <a href="/portfolio">PORTFOLIO</a>
       <a href="/talk">TALK</a>
-    </nav>
+    </Frame>
   );
 }
 
 export default Nav;
+
+const Frame = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 80px;
+  font-size: 25px;
+  line-height: 80px;
+  background-color: rgba(0, 0, 0, 0.5);
+  text-align: right;
+  a {
+    text-decoration: none;
+    color: white;
+    margin-right: 20px;
+  }
+
+  a:hover {
+    text-decoration: underline;
+    color: white;
+  }
+`;
