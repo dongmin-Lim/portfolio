bb2e33b5456505922d727a4420b714f5ccc0f1c4
임동민 <limdongmin@imdongmin-ui-MacBookPro.local>
2023-07-18 18:52:24 +0900

    Fix Home css

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,15 +1,22 @@
 import styles from "./Home.module.css";
+import styled from "styled-components";
 
 function Home() {
   return (
-    <div>
+    <Frame>
       <h1 className={styles.title}>
         프론트엔드 개발자를 꿈꾸고 있는 임동민의 자기소개 페이지 입니다.
       </h1>
       <a className={styles.nextPage} href="/profile">
         GO TO PROFILE
       </a>
-    </div>
+    </Frame>
   );
 }
 export default Home;
+
+const Frame = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+`;
