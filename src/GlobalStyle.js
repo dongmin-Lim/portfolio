370e3bc2ed32cb29396c77341dd68b03b12eeb3c
임동민 <limdongmin@imdongmin-ui-MacBookPro.local>
2023-07-28 14:13:22 +0900

    배경 그라데이션 적용

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,17 +3,11 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 
   body {
-    margin: 0 auto;
     width: 1200px;
-    margin-top: 100px;
-    /* margin-bottom: 50px; */
-    /* background-color: rgb(40, 40, 40); */
-    /* color: rgb(255, 255, 255); */
-    /* background: linear-gradient( to bottom, rgb(150,50,50), black ); */
-    background-repeat: no-repeat;
-    background-size: auto;
-    font-family: "Do Hyeon", sans-serif;
     height: 100vh;
+    margin: 0 auto;
+    margin-top: 100px;
+    font-family: "Do Hyeon", sans-serif;
     background: linear-gradient(
         to bottom,
         rgba(0, 232, 232, 0.8),
